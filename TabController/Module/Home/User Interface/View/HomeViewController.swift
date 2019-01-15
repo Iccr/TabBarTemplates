8a62cb4eb6638ef2345fe02811708d184b246d80
gme_2 <sis.ccr@gmail.com>
2019-01-15 18:12:08 +0900

    change height of text field

--- TabController/Module/Home/User Interface/View/HomeViewController.swift
+++ TabController/Module/Home/User Interface/View/HomeViewController.swift
@@ -49,8 +49,20 @@ class HomeViewController: UIViewController {
     private func setupShadows() {
         [fromDestinationTextField, toDestinationTextField, checkInDateTextField, checkOutDateTextField].forEach({
             $0?.layer.borderWidth = 0.1
-            $0?.layer.addShadow(offset: CGSize.init(width: 1, height: 2.5))
+            $0?.layer.addShadow(offset: CGSize.init(width: 1, height: 1.8))
         })
+        self.addAttributesToTextfield(textfield: fromDestinationTextField, placeholder: "From")
+        self.addAttributesToTextfield(textfield: toDestinationTextField, placeholder: "To")
+        self.addAttributesToTextfield(textfield: checkInDateTextField, placeholder: "Check in")
+        self.addAttributesToTextfield(textfield: checkOutDateTextField, placeholder: "Check out")
+        
+    }
+    
+    private func addAttributesToTextfield(textfield: UITextField?, placeholder: String) {
+        let attributes = [NSAttributedStringKey.foregroundColor: UIColor.lightGray,
+                          .font : UIFont.systemFont(ofSize: 9, weight: .regular)]
+        
+        textfield?.attributedPlaceholder = NSAttributedString(string: placeholder, attributes:attributes)
     }
     
     override func setupTabItem() {
